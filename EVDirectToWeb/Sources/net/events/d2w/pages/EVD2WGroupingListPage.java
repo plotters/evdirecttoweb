f798600d4db7afcdbf7b196d6650a71cf157ebcc
guido.neitzer <guido.neitzer@c80f1718-c537-0410-815c-5f4d545c29e3>
2007-10-21 22:14:10 +0000

    formatted source code

--- EVDirectToWeb/Sources/net/events/d2w/pages/EVD2WGroupingListPage.java
+++ EVDirectToWeb/Sources/net/events/d2w/pages/EVD2WGroupingListPage.java
@@ -1,7 +1,9 @@
 package net.events.d2w.pages;
+
 import com.webobjects.appserver.WOContext;
 
 import er.directtoweb.ERD2WGroupingListPageTemplate;
+
 public class EVD2WGroupingListPage extends ERD2WGroupingListPageTemplate {
 	public EVD2WGroupingListPage(WOContext wocontext) {
 		super(wocontext);
