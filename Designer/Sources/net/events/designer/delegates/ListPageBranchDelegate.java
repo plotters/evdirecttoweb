5d912992ecaa75a9f915a077fd1d6f95d2f78821
guido.neitzer <guido.neitzer@c80f1718-c537-0410-815c-5f4d545c29e3>
2007-10-21 23:32:21 +0000

    Initial import.

---
+++ Designer/Sources/net/events/designer/delegates/ListPageBranchDelegate.java
@@ -0,0 +1,40 @@
+package net.events.designer.delegates;
+
+import com.webobjects.appserver.*;
+import com.webobjects.directtoweb.*;
+import com.webobjects.foundation.*;
+
+import er.extensions.*;
+
+public class ListPageBranchDelegate extends BranchDelegate {
+	
+	public NSArray branchChoicesForContext(D2WContext context) {
+		return super.branchChoicesForContext(context);
+	}
+
+	public WOComponent createNew (WOComponent sender) {
+		String entityName = (String) sender.valueForKeyPath("branch.entityName");
+		if (entityName == null) {
+			entityName = (String) sender.valueForKeyPath("d2wContext.entity.name");
+		}
+
+		EditPageInterface epi = D2W.factory().editPageForNewObjectWithEntityNamed(entityName, sender.session());
+		epi.setNextPage(sender.context().page());
+		
+		try {
+			if (sender.context().page() != null && sender.context().page().valueForKey("object") != null) {
+				ERXGenericRecord nextObject = (ERXGenericRecord) ((WOComponent) epi).valueForKey ("object");
+				ERXGenericRecord thisObject = (ERXGenericRecord) sender.context().page().valueForKey("object");
+			
+				String relationshipName = (String)d2wContext(sender).valueForKey("reverseRelationshipKey");
+				nextObject.addObjectToBothSidesOfRelationshipWithKey(
+						thisObject.localInstanceIn(nextObject.editingContext()), 
+						relationshipName);
+			}
+		}
+		catch (Exception e) {
+		}
+
+		
+		return (WOComponent) epi;	}
+}
