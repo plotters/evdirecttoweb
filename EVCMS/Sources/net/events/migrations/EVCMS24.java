c37b36936ebb38063a8c567d7c859cdd61097f87
guido.neitzer <guido.neitzer@c80f1718-c537-0410-815c-5f4d545c29e3>
2007-10-24 04:18:43 +0000

    Added date to DataSetEntry, can be pushed by a DataSetDateItem, added search for date range on Medical Data Set Entry

---
+++ EVCMS/Sources/net/events/migrations/EVCMS24.java
@@ -0,0 +1,11 @@
+package net.events.migrations;
+
+import er.extensions.migration.*;
+
+public class EVCMS24 extends ERXMigration {
+
+	protected String migrationBundleName() {
+		return "EVCMS";
+	}
+
+}
