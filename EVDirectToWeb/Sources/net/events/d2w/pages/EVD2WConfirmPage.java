1eff9a0932f5a1dc45ecb5bbef619084eeed39bf
guido.neitzer <guido.neitzer@c80f1718-c537-0410-815c-5f4d545c29e3>
2007-10-29 04:49:40 +0000

    some cleanup

--- EVDirectToWeb/Sources/net/events/d2w/pages/EVD2WConfirmPage.java
+++ EVDirectToWeb/Sources/net/events/d2w/pages/EVD2WConfirmPage.java
@@ -1,7 +1,9 @@
 package net.events.d2w.pages;
+
 import com.webobjects.appserver.WOContext;
 
 import er.directtoweb.ERD2WConfirmPageTemplate;
+
 public class EVD2WConfirmPage extends ERD2WConfirmPageTemplate {
 	public EVD2WConfirmPage(WOContext wocontext) {
 		super(wocontext);
