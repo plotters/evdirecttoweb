618af7d3689dd93b1f649f1e5583d82222795263
guido.neitzer <guido.neitzer@c80f1718-c537-0410-815c-5f4d545c29e3>
2008-08-21 06:18:07 +0000

    Let this inherit from ERDCustomEditComponent, so we don't get that TinyMCE crap in

--- trunk/EVDirectToWeb/Sources/net/events/d2w/components/EVD2WEditHTML.java
+++ trunk/EVDirectToWeb/Sources/net/events/d2w/components/EVD2WEditHTML.java
@@ -3,27 +3,51 @@ package net.events.d2w.components;
 import net.events.d2w.extensions.EVD2WApplication;
 
 import com.webobjects.appserver.WOContext;
-import com.webobjects.appserver.WOResponse;
 
-import er.directtoweb.components.strings.ERDEditHTML;
+import er.directtoweb.components.ERDCustomEditComponent;
 
-public class EVD2WEditHTML extends ERDEditHTML {
+/**
+ * Very basic rich text editor that uses FCKEditor
+ * 
+ * @author cug
+ *
+ */
+public class EVD2WEditHTML extends ERDCustomEditComponent {
 	
+	/**
+	 * Id for the textfield
+	 */
 	private String id = null;
 	
+    /**
+     * Standard constructor
+     * 
+     * @param context
+     * @author cug
+     */
     public EVD2WEditHTML(WOContext context) {
         super(context);
     }
     
+    /**
+     * Just set the id to null
+     *  
+     * @see er.directtoweb.components.ERDCustomEditComponent#reset()
+     * 
+     * @author cug - Aug 21, 2008
+     */
     public void reset () {
     	this.id = null;
     	super.reset();
     }
     
-    public void appendToResponse (WOResponse response, WOContext context) {
-    	super.appendToResponse(response, context);
-    }
-    
+    /**
+     * ID for the editor
+     * 
+     * @return
+     * 
+     * @author cug - Aug 21, 2008
+     */
     public String editorId () {
     	if (this.id == null) {
     		this.id = "fck" + ((EVD2WApplication) this.application()).randomInteger();
